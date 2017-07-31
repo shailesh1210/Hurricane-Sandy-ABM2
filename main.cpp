4fa1777ef94f3025cff5ec36f6ae7762ffcf3a6a
shailesh1210 <shaileshtamrakar@gmail.com>
2017-07-31 10:22:34 -0400

    full screen window

--- main.cpp
+++ main.cpp
@@ -8,8 +8,10 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	QRect rec = QApplication::desktop()->screenGeometry();
 	Skeleton w;
-	//w.setFixedSize(240,280);
+	w.resize(rec.width(), rec.height()-75);
+	//w.setFixedSize(2000, 1000);
 	w.setWindowTitle("Sandy ABM");
 	w.show();
 	return a.exec();
